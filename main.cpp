411d6208540d604ff589e98ef76c6b8071dcde72
Nathan J. <jacquinet.nathan@gmail.com>
2024-06-08 01:47:43 +0200

    ajout modules kompute glfw glm

--- main.cpp
+++ main.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 
-
-#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 #include "utils/shader.hpp"
@@ -29,54 +27,9 @@ int main() {
         std::cerr << "Error while create Window\n";
     }
 
-    ///////////////////////////////////////////////////:
-    GLuint VertexArrayID;
-    glGenVertexArrays(1, &VertexArrayID);
-    glBindVertexArray(VertexArrayID);
-
-    // Create and compile our GLSL program from the shaders
-    GLuint programID = LoadShaders( "SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader" );
-
-    static const GLfloat g_vertex_buffer_data[] = {
-            -1.0f, -1.0f, 0.0f,
-            1.0f, -1.0f, 0.0f,
-            0.0f,  1.0f, 0.0f,
-    };
-
-    GLuint vertexbuffer;
-    glGenBuffers(1, &vertexbuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
-
-
     while (!glfwWindowShouldClose(window))
     {
-        // Clear the screen
-        glClear( GL_COLOR_BUFFER_BIT );
-
-        // Use our shader
-        glUseProgram(programID);
-
-        // 1rst attribute buffer : vertices
-        glEnableVertexAttribArray(0);
-        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-        glVertexAttribPointer(
-                0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
-                3,                  // size
-                GL_FLOAT,           // type
-                GL_FALSE,           // normalized?
-                0,                  // stride
-                (void*)0            // array buffer offset
-        );
-
-        // Draw the triangle !
-        glDrawArrays(GL_TRIANGLES, 0, 3); // 3 indices starting at 0 -> 1 triangle
-
-        glDisableVertexAttribArray(0);
-
-        // Swap buffers
-        glfwSwapBuffers(window);
-        glfwPollEvents();
+        
     }
 
     glfwDestroyWindow(window);
