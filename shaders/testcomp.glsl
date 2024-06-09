8ff70690697dc9257bd33ac31a7dcf3f94449306
Nathan J. <jacquinet.nathan@gmail.com>
2024-06-09 23:06:50 +0200

    ajout shaders test

--- shaders/testcomp.glsl
+++ shaders/testcomp.glsl
@@ -1,17 +1,23 @@
 #version 450
 
-layout(push_constant) uniform PushConstants {
-    float x;
-    float y;
-    float z;
-} pcs;
-
 layout (local_size_x = 1) in;
 
-layout(set = 0, binding = 0) buffer a { float pa[]; };
+// The input tensors bind index is relative to index in parameter passed
+layout(set = 0, binding = 0) buffer buf_in_a { float in_a[]; };
+layout(set = 0, binding = 1) buffer buf_in_b { float in_b[]; };
+layout(set = 0, binding = 2) buffer buf_out_a { uint out_a[]; };
+layout(set = 0, binding = 3) buffer buf_out_b { uint out_b[]; };
+
+// Kompute supports push constants updated on dispatch
+layout(push_constant) uniform PushConstants {
+    float val;
+} push_const;
+
+// Kompute also supports spec constants on initalization
+layout(constant_id = 0) const float const_one = 0;
 
 void main() {
-    pa[0] = pa[0] + pcs.x;
-    pa[1] = pa[1] + pcs.y;
-    pa[2] = pa[1] + pcs.z;
+    uint index = gl_GlobalInvocationID.x;
+    out_a[index] += uint( in_a[index] * in_b[index] );
+    out_b[index] += uint( const_one * push_const.val );
 }
