31372411b3f11602b839e3e935fb4a9c339abb57
Nathan J. <jacquinet.nathan@gmail.com>
2024-06-09 23:06:36 +0200

    ajout readfile shader spirv

---
+++ engine/utils.cpp
@@ -0,0 +1,24 @@
+//
+// Created by Nathan on 08/06/2024.
+//
+
+#include <fstream>
+#include "utils.h"
+
+std::vector<char> Utils::readFile(const std::string& filename) {
+    std::ifstream file(filename, std::ios::ate | std::ios::binary);
+
+    if (!file.is_open()) {
+        throw std::runtime_error("failed to open the file" + filename + "!");
+    }
+
+    size_t fileSize = (size_t) file.tellg();
+    std::vector<char> buffer(fileSize);
+
+    file.seekg(0);
+    file.read(buffer.data(), fileSize);
+
+    file.close();
+
+    return buffer;
+}
---
+++ engine/utils.h
@@ -0,0 +1,19 @@
+//
+// Created by Nathan on 08/06/2024.
+//
+
+#ifndef IFS_UTILS_H
+#define IFS_UTILS_H
+
+
+#include <vector>
+#include <string>
+
+class Utils {
+
+public:
+    static std::vector<char> readFile(const std::string& filename);
+};
+
+
+#endif //IFS_UTILS_H
