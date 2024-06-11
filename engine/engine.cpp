0e30fef3a5082ebbca56b7a22d710043cdadce66
gibus21250 <jacquinet.nathan@gmail.com>
2024-06-11 16:29:01 +0200

    push ifs bugué

--- engine/engine.cpp
+++ engine/engine.cpp
@@ -94,6 +94,7 @@ void Engine::run() {
 
 void Engine::destroy()
 {
+
     cleanup();
 }
 
@@ -667,7 +668,7 @@ void Engine::createGraphicsPipeline() {
     rasterizer.polygonMode = VK_POLYGON_MODE_FILL;
     rasterizer.lineWidth = 1.0f;
     rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
-    rasterizer.frontFace = VK_FRONT_FACE_CLOCKWISE;
+    rasterizer.frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE;
     rasterizer.depthBiasEnable = VK_FALSE;
 
     VkPipelineMultisampleStateCreateInfo multisampling{};
--- main.cpp
+++ main.cpp
@@ -4,9 +4,11 @@
 #include <kompute/Kompute.hpp>
 
 #include "engine/engine.h"
-#include "engine/utils.h"
 
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+void IFS(std::vector<glm::vec2>& object, std::vector<glm::mat3>& transorms, uint32_t nbIteration);
 
 int main()
 {
@@ -16,17 +18,67 @@ int main()
     e.initEngine();
 
     //Triangle
-    const std::vector<Engine::Vertex> vertices = {
-            {{0.0f, -0.5f}},
-            {{0.5f, 0.5f}},
-            {{-0.5f, 0.5f}}
+    std::vector<glm::vec2> vertices = {
+            {0.0f, -0.7f},
+            {-0.5f, 0.5f},
+            {0.5f, 0.5f}
     };
 
-    e.createBuffer((void *) vertices.data(), 3 * sizeof(glm::vec2), 3, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
+
+    std::vector<glm::mat3> transforms =
+    {
+        glm::mat3(0.5, 0, 0,
+        0, 0.5, -0.25,
+        0, 0, 1),
+
+        glm::mat3(0.5, 0, -0.25,
+                  0, 0.5, 0.25,
+                  0, 0, 1),
+
+        glm::mat3(0.5, 0, 0.25,
+                  0, 0.5, 0.25,
+                  0, 0, 1),
+
+    };
+
+
+    IFS(vertices, transforms, 1);
+
+
+    e.createBuffer((void *) vertices.data(),  vertices.size() * sizeof(glm::vec2), vertices.size(), VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
 
 
     e.run();
-
     e.destroy();
 
+}
+
+//Transform are 3D matrices bc of heterogenous coord
+void IFS(std::vector<glm::vec2>& object, std::vector<glm::mat3>& transorms, uint32_t nbIteration)
+{
+    uint32_t padding = object.size();
+    uint32_t nbMaxPoints = padding;
+
+    for (int i = 0; i < nbIteration; ++i) {
+        nbMaxPoints *= transorms.size();
+    }
+
+    object.resize(nbMaxPoints);
+
+    //For each iteration
+    for (int i = 0; i < nbIteration; ++i)
+    {
+
+        //For each transform, we write result from the end to the start of the buffer
+        for (int j = 0; j < transorms.size(); ++j)
+        {
+            //We apply tranform for each point
+            for (int k = 0; k < padding; ++k)
+            {
+                object[((transorms.size() - 1 + j) * padding) + k] = transorms[j] * glm::vec3(object[k], 0.0);
+            }
+        }
+        //Update size of the total object
+        padding = padding * transorms.size();
+    }
 }
