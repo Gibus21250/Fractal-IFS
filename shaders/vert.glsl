3e1c040906c575e656b2503cb5567bcea0c5595f
Nathan J. <jacquinet.nathan@gmail.com>
2024-06-15 23:28:16 +0200

    change color frag

--- shaders/vert.glsl
+++ shaders/vert.glsl
@@ -6,5 +6,5 @@ layout(location = 0) out vec3 fragColor;
 
 void main() {
     gl_Position = vec4(inPosition, 0.0, 1.0);
-    fragColor = vec3(1.0, 0.0, 0.0);
+    fragColor = vec3(0.5, (inPosition+1)/2);
 }
