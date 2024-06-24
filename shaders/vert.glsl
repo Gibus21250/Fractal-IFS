3271cf560f1425e9335eaf2395300120ff46b5c5
gibus21250 <jacquinet.nathan@gmail.com>
2024-06-24 14:29:51 +0200

    ajout pushconstant

--- shaders/vert.glsl
+++ shaders/vert.glsl
@@ -4,7 +4,14 @@ layout(location = 0) in vec2 inPosition;
 
 layout(location = 0) out vec3 fragColor;
 
+//push constants block
+layout(push_constant) uniform constants
+{
+    vec4 data;
+    mat4 render_matrix;
+} PushConstants;
+
 void main() {
-    gl_Position = vec4(inPosition, 0.0, 1.0);
+    gl_Position = PushConstants.render_matrix * vec4(inPosition, 0.0, 1.0);
     fragColor = vec3(0.5, (inPosition+1)/2);
 }
