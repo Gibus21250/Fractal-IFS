7e420bfebf69fe3617aef01ce6e370340c1a2fc0
gibus21250 <jacquinet.nathan@gmail.com>
2024-06-24 14:28:32 +0200

    camera class added

---
+++ engine/camera.cpp
@@ -0,0 +1,68 @@
+//
+// Created by nj123548 on 24/06/24.
+//
+
+#include "camera.h"
+
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/transform.hpp>
+
+glm::mat4 Camera::getVPMatrix() const
+{
+    glm::mat4 view = glm::lookAt(this->position, this->lookAt, this->up);
+    glm::mat4 projection = glm::perspective(glm::radians(this->fov), this->aspect, 0.1f, 200.0f);
+
+    return projection * view;
+}
+
+//----------------------------------------------------------\\
+
+Camera::Camera()
+    : position(0, 0, -1), lookAt(0, 0, 0), up(0, 1, 0), fov(90), aspect(16.0f/10) {}
+
+Camera::Camera(glm::vec3 pos, glm::vec3 lookAt, glm::vec3 up)
+    : position(pos), lookAt(lookAt), up(up), fov(80), aspect(16.0f/9) {}
+
+Camera::Camera(glm::vec3 pos, glm::vec3 lookAt, glm::vec3 up, float fov, float aspect)
+    : position(pos), lookAt(lookAt), up(up), fov(fov), aspect(aspect) {}
+
+const glm::vec3 &Camera::getPosition() const {
+    return position;
+}
+
+void Camera::setPosition(const glm::vec3 &nPosition) {
+    Camera::position = nPosition;
+}
+
+const glm::vec3 &Camera::getLookAt() const {
+    return lookAt;
+}
+
+void Camera::setLookAt(const glm::vec3 &nLookAt) {
+    Camera::lookAt = nLookAt;
+}
+
+const glm::vec3 &Camera::getUp() const {
+    return up;
+}
+
+void Camera::setUp(const glm::vec3 &nUp) {
+    Camera::up = nUp;
+}
+
+float Camera::getFov() const {
+    return fov;
+}
+
+void Camera::setFov(float fov) {
+    Camera::fov = fov;
+}
+
+float Camera::getAspect() const {
+    return aspect;
+}
+
+void Camera::setAspect(float aspect) {
+    Camera::aspect = aspect;
+}
+
---
+++ engine/camera.h
@@ -0,0 +1,44 @@
+//
+// Created by nj123548 on 24/06/24.
+//
+
+#ifndef IFS_CAMERA_H
+#define IFS_CAMERA_H
+
+#include <glm/glm.hpp>
+
+class Camera {
+
+public:
+    Camera();
+    Camera(glm::vec3 pos, glm::vec3 lookAt, glm::vec3 up);
+    Camera(glm::vec3 pos, glm::vec3 lookAt, glm::vec3 up, float fov, float aspect);
+
+    const glm::vec3 &getPosition() const;
+    void setPosition(const glm::vec3 &position);
+
+    const glm::vec3 &getLookAt() const;
+    void setLookAt(const glm::vec3 &lookAt);
+
+    const glm::vec3 &getUp() const;
+    void setUp(const glm::vec3 &up);
+
+    float getFov() const;
+    void setFov(float fov);
+
+    float getAspect() const;
+    void setAspect(float aspect);
+
+    //---------------------------------------------------------------\\
+
+    glm::mat4 getVPMatrix() const;
+
+private:
+    glm::vec3 position, lookAt, up{0, -1, 0};
+    float fov;
+    float aspect;
+
+};
+
+
+#endif //IFS_CAMERA_H
