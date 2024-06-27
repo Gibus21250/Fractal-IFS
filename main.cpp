8172d29386e4da025f13c93495d6f6e903b4439a
gibus21250 <jacquinet.nathan@gmail.com>
2024-06-27 16:02:07 +0200

    add compute IFS on GPU

--- main.cpp
+++ main.cpp
@@ -10,17 +10,17 @@
 
 #include <GLFW/glfw3.h>
 
-size_t iterations = 0;
-Engine e;
+size_t g_iterations = 0;
+Engine g_engine;
 
-std::vector<glm::vec2> init = {
+std::vector<glm::vec2> g_init = {
         glm::vec2(0.0, -1.0),
         glm::vec2(-1.0f, 1.0f),
         glm::vec2(1.0f, 1.0f)
 };
 
 
-std::vector<glm::mat3> transforms =
+std::vector<glm::mat3> g_transforms =
         {
                 glm::mat3(0.5, 0, 0,
                           0, 0.5, -0.5,
@@ -36,29 +36,59 @@ std::vector<glm::mat3> transforms =
 
         };
 
+bool g_CPU = false;
+
+void* g_uniformBuffer = nullptr;
+
 void IFS(glm::vec2* object, size_t nb, std::vector<glm::mat3>& transforms, uint32_t nbIteration);
-void drawIFS(std::vector<glm::vec2>& init, std::vector<glm::mat3>& transforms, uint32_t iter);
+void computeIFS_CPU(std::vector<glm::vec2>& init, std::vector<glm::mat3>& transforms, uint32_t iter);
+void computeIFS_GPU(std::vector<glm::vec2>& init, std::vector<glm::mat3>& transforms, uint32_t iter);
 
 void keyboard_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
     if (key == GLFW_KEY_KP_ADD && action == GLFW_PRESS)
     {
-        iterations++;
-        e.clearDrawableObjects();
-        drawIFS(init, transforms, iterations);
+
+        g_iterations++;
+        g_engine.clearDrawableObjects();
+        if(g_CPU)
+            computeIFS_CPU(g_init, g_transforms, g_iterations);
+        else
+        {
+            g_uniformBuffer = nullptr;
+            computeIFS_GPU(g_init, g_transforms, g_iterations);
+        }
+
     }
     else if(key == GLFW_KEY_KP_SUBTRACT && action == GLFW_PRESS)
     {
-        if(iterations)
+        if(g_iterations)
         {
-            iterations--;
-            e.clearDrawableObjects();
-            drawIFS(init, transforms, iterations);
+            g_iterations--;
+            g_engine.clearDrawableObjects();
+            g_uniformBuffer = nullptr;
+            if(g_CPU)
+                computeIFS_CPU(g_init, g_transforms, g_iterations);
+            else
+                computeIFS_GPU(g_init, g_transforms, g_iterations);
         }
     }
     else if(key == GLFW_KEY_R && action == GLFW_PRESS)
     {
-        e.switchConservativeRaster();
+        g_engine.switchConservativeRaster();
+    }
+    else if(key == GLFW_KEY_S && action == GLFW_PRESS)
+    {
+        g_CPU = !g_CPU;
+        g_engine.clearDrawableObjects();
+        if(g_CPU)
+            computeIFS_CPU(g_init, g_transforms, g_iterations);
+        else
+        {
+            g_uniformBuffer = nullptr;
+            computeIFS_GPU(g_init, g_transforms, g_iterations);
+        }
+
     }
 }
 
@@ -66,36 +96,36 @@ int main()
 {
 
     //Initialize small render engine
-    e.initEngine();
-    glfwSetKeyCallback(e.getGLFWindow(), keyboard_callback);
+    g_engine.initEngine();
+    glfwSetKeyCallback(g_engine.getGLFWindow(), keyboard_callback);
+    computeIFS_GPU(g_init, g_transforms, g_iterations);
 
-    drawIFS(init, transforms, iterations);
-
-    e.run();
-    e.destroy();
+    g_engine.run();
+    g_engine.destroy();
 }
 
-void drawIFS(std::vector<glm::vec2>& ini, std::vector<glm::mat3>& trans, uint32_t iter)
+void computeIFS_CPU(std::vector<glm::vec2>& init, std::vector<glm::mat3>& transforms, uint32_t iter)
 {
-    size_t nbMaxPoints = ini.size();
+    size_t nbMaxPoints = init.size();
     for (int i = 0; i < iter; ++i) {
-        nbMaxPoints *= trans.size();
+        nbMaxPoints *= transforms.size();
     }
 
     std::cout << "Allocation needed: " << nbMaxPoints * sizeof(glm::vec2) << " bytes\n";
-    auto* buff = (glm::vec2*) e.createBuffer(nbMaxPoints * sizeof(glm::vec2), VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
+    auto* buff = (glm::vec2*) g_engine.createBuffer(nbMaxPoints * sizeof(glm::vec2), VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
 
-    memcpy(buff, ini.data(), sizeof(glm::vec2) * ini.size());
+    memcpy(buff, init.data(), sizeof(glm::vec2) * init.size());
 
     auto t1 = std::chrono::high_resolution_clock::now();
-    IFS(buff, 3, trans, iter);
+    IFS(buff, 3, transforms, iter);
     auto t2 = std::chrono::high_resolution_clock::now();
 
     std::cout << "Time spent: " << t2 - t1 << "\n";
 
     std::vector<void*> res = {buff};
 
-    e.addDrawableObject(res, nbMaxPoints);
+
+    g_engine.addDrawableObject(res, nbMaxPoints);
 }
 
 //Transform are 3D matrices bc of heterogeneous coord
@@ -117,4 +147,32 @@ void IFS(glm::vec2* object, size_t nbVertices, std::vector<glm::mat3>& trans, ui
         //Update size of the total object
         nbVertices = nbVertices * trans.size();
     }
+}
+
+void computeIFS_GPU(std::vector<glm::vec2>& init, std::vector<glm::mat3>& transforms, uint32_t iter)
+{
+    //Si l'uniform buffer n'a pas été initialisé
+    if(g_uniformBuffer == nullptr)
+        g_uniformBuffer = g_engine.createBuffer(3 * sizeof(glm::mat4), VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
+
+    //On met à jour l'uniformbuffer des transforms
+    memcpy(g_uniformBuffer, transforms.data(), transforms.size() * sizeof(glm::mat3));
+
+    //On met à jour le pushconstant
+    g_engine.pushConstants.nbTransformation = transforms.size();
+    g_engine.pushConstants.nbIteration = iter;
+    size_t nbMaxInstance = 1;
+    for (int i = 0; i < iter; ++i) {
+        nbMaxInstance *= transforms.size();
+    }
+    g_engine.pushConstants.maxInstance = nbMaxInstance;
+
+    //On crée un vertexBuffer de l'élément initial
+    auto* buff = (glm::vec2*) g_engine.createBuffer(init.size() * sizeof(glm::vec2), VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
+
+    memcpy(buff, init.data(), init.size() * sizeof(glm::vec2));
+    std::vector<void*> res = {buff, g_uniformBuffer};
+
+    //On ajoute un objet drawable de l'élément de base, mais nbInstances fois
+    g_engine.addDrawableObject(res, 3, nbMaxInstance);
 }
